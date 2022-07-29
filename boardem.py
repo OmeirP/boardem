bd62bfb6e37fd9ae979fcd2dd8e7f95c1537a643
Omeir <50913215+OmeirP@users.noreply.github.com>
2022-07-29 22:48:10 +0100

    cleaned up counter coordinate management

--- boardem.py
+++ boardem.py
@@ -6,7 +6,7 @@ import time
 blue=(0,0,255)
 gridBlu=(80, 101, 135) #for grid
 red=(255,0,0)
-purple=(136, 0, 255) #for player 1
+purple=(154, 74, 224) #for player 1
 greyBG=(48,48,48) #for background
 sprngGrn=(0, 255, 149) #for player 2
 
@@ -20,6 +20,16 @@ display_height = 1200
 
 gameDisplay=pygame.display.set_mode((display_width,display_height))
 
+
+
+#p1StrtCoords=(370,680)
+#p2StrtCoords=(370,700)
+
+
+p1Coords=(370,680)
+p2Coords=(370,700)
+
+
 def drawGrid():
     tileSize=60
     for x in range(340, (display_width-340), tileSize):
@@ -28,8 +38,8 @@ def drawGrid():
             pygame.draw.rect(gameDisplay, gridBlu, rect, 1)
             
 def drawCountrs():
-    pygame.draw.circle(gameDisplay, purple, (370,680), 10)
-    pygame.draw.circle(gameDisplay, sprngGrn, (370,700), 10)
+    pygame.draw.circle(gameDisplay, purple, p1Coords, 10)
+    pygame.draw.circle(gameDisplay, sprngGrn, p2Coords, 10)
     
 
 
@@ -39,10 +49,11 @@ def game_loop():
 
     gameExit = False
     
-    p1yStartPos=690
-    p2yStartPos=710
+
     cntrChange=60
-    #set counter diamter at 20
+    #counter diameter is 20
+    
+    
     
     
     
